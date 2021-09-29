4e8d8256f3ff89264b981e759b77cace63fcde5e
Batman-S <83809552+Batman-S@users.noreply.github.com>
2021-09-29 21:41:38 +0900

    fixed and tested CRUD routes on users

--- server/Model.js
+++ server/Model.js
@@ -1,13 +1,10 @@
-// Import mongoose
 const mongoose = require("mongoose");
-// Declare schema and assign Schema class
 const Schema = mongoose.Schema;
-// Create Schema Instance and add schema propertise
 const userSchema = new Schema({
   name: {
     type: String,
     required: true,
   },
 });
-// create and export model
+
 module.exports = mongoose.model("user", userSchema);
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,7 @@
 // create App function
 module.exports = function (app) {
-  var users = require("./controller");
-  // users Routes
-  // get and post request for /users endpoints
-  // app.route("/users").get(users.listAllUsers).post(users.createNewUser);
-  // put and delete request for /users endpoints
-  //   app.route("/user/:id").put(users.updateUser).delete(users.deleteUser);
+  const users = require("./controller");
+
+  app.route("/users").get(users.listAllUsers).post(users.createNewUser);
+  app.route("/users/:id").patch(users.updateUser).delete(users.deleteUser);
 };
