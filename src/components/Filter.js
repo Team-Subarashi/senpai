22a31a0621a63e3a007b927848b3e32e491dfae7
gonuma <81454703+gonuma@users.noreply.github.com>
2021-10-01 19:27:15 +0900

    Minor change to get on GCP

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -31,7 +31,7 @@ export default function Filter() {
         }}
         onClick={() => toggleMenuState(!menuState)}
       >
-        Category
+        Cat
       </Button>
       <Menu
         open={menuState}
