be957f95ca8bda09f8e466b95ecf1b969b0b9d53
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-10-01 10:56:42 +0900

    chore added navBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
+//import './index.css'
 import './App.css';
-import NavBar from "./Components/NavBar"
-import Splash from "./Pages/Splash"
-import Profile from "./Pages/Profile"
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Splash from "./pages/Splash"
+import Profile from "./pages/Profile"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import NavBar from './components/NavBar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundColor: '#616161',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: 'cover',
+  }
+}));
 
 function App() {
+  const classes = useStyles()
   return (
     // <Router>
-      <div className="App">
-        <NavBar />
-          {/* <Route path="/home">  */}
-            <Splash />
-          {/* </Route> */}
-          {/* <Route path="/profile"> */}
-            <Profile />
-          {/* </Route> */}
-      </div>
+    <div className={classes.root}>
+      <CssBaseline />
+
+      <NavBar />
+
+
+
+      <Splash />
+      {/* </Route> */}
+      {/* <Route path="/profile"> */}
+      <Profile />
+      {/* </Route> */}
+
+    </div>
     // </Router>
   );
 }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar } from '@material-ui/core'
+import logo from '../logo/logo_cropped.png'
 
-export default function NavBar() {
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '40vh'
+    },
+    appbar: {
+        backgroundColor: '#673AB7',
+        fontFamily: 'Nunioto'
+    },
+    appbarWrapper: {
+        width: '80%',
+        margin: '0 auto'
+    }
+}))
+
+const NavBar = () => {
+    const classes = useStyles()
     return (
-        <div>
-            NavBar
+        <div className={classes.root}>
+            <AppBar className={classes.appbar} elevation={0}>
+                <Toolbar className={classes.appbarWrapper}>
+                    <h1><img src={logo} alt="senpai" height="36" width="auto" /></h1>
+                </Toolbar>
+            </AppBar>
+            <div>
+                <h1>Welcome to <br /> Senpai</h1>
+            </div>
+
         </div>
     )
 }
+
+export default NavBar
