bd657240f246c22ee556f8bb48d51d0e651abf87
Batman-S <83809552+Batman-S@users.noreply.github.com>
2021-10-01 21:28:33 +0900

    added files model and controller, and endpoints

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,14 @@
 // create App function
 module.exports = function (app) {
-  const users = require("./controller");
+  const users = require("./controllers/userController");
+  const files = require("./controllers/fileController");
 
   app.route("/users").get(users.listAllUsers).post(users.createNewUser);
   app.route("/users/:id").patch(users.updateUser).delete(users.deleteUser);
+
+  app.route("/files").get(files.listAllFiles).post(files.createNewFile);
+  app.route("/files/:id").patch(files.updateFile).delete(files.deleteFile);
+
+
+
 };
