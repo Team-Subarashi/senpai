80c95d9c52c8cd24d9e45eaaffcff22896ca2fec
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-10-06 16:12:10 +0900

    modified server/index.js due to add socket.io

--- server/index.js
+++ server/index.js
@@ -7,8 +7,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 const routes = require("./routes");
 const http = require('http');
+const { Server } = require("socket.io")
+
 
 const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+})
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id)
+  })
+})
 
 require("dotenv").config();
 require("./config.js"); // Import DB Connection
