c76311386f59b4bd5f96dc9787665b9fdd101e46
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-10-06 20:58:03 +0900

    making a room

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import MessageList from '../components/MessageList'
+import React from 'react';
+import MessageList from '../components/MessageList';
+import io from 'socket.io-client';
+import { useState } from 'react';
+
+const socket = io.connect("http://localhost:3001")
 
 const Messages = () => {
+    const [userName, setUserName] = useState("");
+    const [room, setRoom] = useState("");
+
+
+    const joinRoom = () => {
+
+    };
+
+
     return (
         <div>
             <MessageList />
+            <h3>🐱Join A Chat🐱</h3>
+            <input type="text" placeholder="John..." onChange={(event) => (event.target.value)} />
+            <input type="text" placeholder="Room ID..." />
+            <button>Join A Room</button>
         </div>
     )
 }
