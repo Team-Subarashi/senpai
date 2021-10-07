648645bd34a6abaad0272393ff061295b39670e1
GiveKumaAGun <reijitsusaka@gmail.com>
2021-10-07 12:53:58 +0900

    Updated theme

--- src/units/theme.js
+++ src/units/theme.js
@@ -3,13 +3,32 @@ import { createTheme } from '@material-ui/core'
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#673AB7'
-    },
-    secondary: {
-      main: '#EEEEEE'
+      main: '#673AB7',
+      secondary: '#616161',
+      contrastText: '#EEEEEE'
     },
     type: 'dark'
   },
+  shape: {
+    // borderRadius: "4px"
+  },
+  spacing: 4,
+  text : {
+    primary: "#fff",
+    secondary: "rgba(255,255,255,0.7)"
+  },
+  typography: {
+    htmlFontSize: 16,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightMedium: 400,
+    fontWeightRegular: 500,
+    fontWeightBold: 700,
+    h1: {
+      fontFamily: "Nunito",
+    }
+  
+  }
 })
 
 export default theme;
