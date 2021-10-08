5dd7f749b12626e34058c7c828d3d3a27734e435
GiveKumaAGun <reijitsusaka@gmail.com>
2021-10-08 12:22:17 +0900

    Chore:transitioning to Mui v5

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { makeStyles } from '@mui/styles';
+import Container from '@mui/material/Container';
 import { getAuth, signInWithEmailAndPassword } from '@firebase/auth';
 import { Link, useHistory } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
+import Grid from '@mui/material/Grid';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -80,7 +79,6 @@ export default function SignIn() {
   return (
 
     <Container component="main" maxWidth="xs">
-      <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
