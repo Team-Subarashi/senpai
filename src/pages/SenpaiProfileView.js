9aa3455e271b14fb6d2687b8c2c3d7cd85bff5a4
GiveKumaAGun <reijitsusaka@gmail.com>
2021-10-11 15:14:30 +0900

    Fix:senpai profile page is now directly accessible

--- src/pages/SenpaiProfileView.js
+++ src/pages/SenpaiProfileView.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Container from "@material-ui/core/Container";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 const useStyles = makeStyles(() => ({
   container: {
@@ -32,145 +34,166 @@ const useStyles = makeStyles(() => ({
 
 export default function SenpaiProfileView({ match, location }) {
   const classes = useStyles();
-  const senpai = location.state.senpai;
+  const [senpai, setSenpai] = useState(location.state ? location.state.senpai : null)
+  
+  const fetchData = async () => {
+    const response = await axios.get('/api/v1/users/' + match.params.id)
+    console.log('ollo')
+    console.log(response)
+    if (response.data) {
+      setSenpai(response.data)
+    }
+  }
+
+  useEffect(() => {
+    console.log('test')
+    console.log(senpai)
+    fetchData()
+  }, [])
+
 
   return (
     <div>
-      <Grid container spacing={3} className={classes.root}>
-        <Grid
-          className={classes.container}
-          item
-          xs={3}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            height: "100%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography variant="h2">{senpai.name}</Typography>
-          <Grid className={classes.avatarHolder} container>
-            <Grid>
-              <Avatar
-                className="senpai-photo"
-                alt="senpai"
-                src={senpai.avatar}
-                style={{ width: 150, height: 150 }}
-              />
-            </Grid>
-          </Grid>
-          <Button variant="contained" color="primary">
-            <Link
-              style={{ color: "white" }}
-              to={`/senpai/${match.params.id}/schedule`}
-            >
-              View schedule
-            </Link>
-          </Button>
+      {senpai ? 
+        <Grid container spacing={3} className={classes.root}>
           <Grid
-            container
-            style={{ alignItems: "center", padding: "1rem", textAlign: "left" }}
+            className={classes.container}
+            item
+            xs={3}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
           >
-            {senpai.twitter ||
+            <Typography variant="h2">{senpai.name}</Typography>
+            <Grid className={classes.avatarHolder} container>
+              <Grid>
+                <Avatar
+                  className="senpai-photo"
+                  alt="senpai"
+                  src={senpai.avatar}
+                  style={{ width: 150, height: 150 }}
+                />
+              </Grid>
+            </Grid>
+            <Button variant="contained" color="primary">
+              <Link
+                style={{ color: "white" }}
+                to={`/senpai/${match.params.id}/schedule`}
+              >
+              View schedule
+              </Link>
+            </Button>
+            <Grid
+              container
+              style={{ alignItems: "center", padding: "1rem", textAlign: "left" }}
+            >
+              {senpai.twitter ||
             senpai.linkedIn ||
             senpai.facebook ||
             senpai.instagram ? (
-                <Typography variant="h5">Socials:</Typography>
+                  <Typography variant="h5">Socials:</Typography>
+                ) : null}
+              <div>
+                <Typography variant="h6">
+                  {senpai.twitter ? (
+                    <a>
+                      <TwitterIcon />
+                    </a>
+                  ) : null}
+                  {senpai.linkedIn ? (
+                    <a>
+                      <LinkedInIcon />{" "}
+                    </a>
+                  ) : null}
+                  {senpai.facebook ? (
+                    <a>
+                      <FacebookIcon />
+                    </a>
+                  ) : null}
+                  {senpai.instagram ? (
+                    <a>
+                      <InstagramIcon />
+                    </a>
+                  ) : null}
+                </Typography>
+              </div>
+              {senpai.email ? (
+                <div>
+                  <Typography variant="h5">Email:</Typography>
+                  <Typography variant="h6">
+                    <a href={senpai.email}>{senpai.email}</a>
+                  </Typography>
+                </div>
               ) : null}
-            <div>
-              <Typography variant="h6">
-                {senpai.twitter ? (
-                  <a>
-                    <TwitterIcon />
-                  </a>
-                ) : null}
-                {senpai.linkedIn ? (
-                  <a>
-                    <LinkedInIcon />{" "}
-                  </a>
-                ) : null}
-                {senpai.facebook ? (
-                  <a>
-                    <FacebookIcon />
-                  </a>
-                ) : null}
-                {senpai.instagram ? (
-                  <a>
-                    <InstagramIcon />
-                  </a>
-                ) : null}
-              </Typography>
-            </div>
-            {senpai.email ? (
-              <div>
-                <Typography variant="h5">Email:</Typography>
-                <Typography variant="h6">
-                  <a href={senpai.email}>{senpai.email}</a>
-                </Typography>
-              </div>
-            ) : null}
-            {senpai.location ? (
-              <div>
-                <Typography variant="h5">Location:</Typography>
-                <Typography variant="h6">{senpai.location}</Typography>
-              </div>
-            ) : null}
-            {senpai.website ? (
-              <div>
-                <Typography variant="h5">Personal Website:</Typography>
-                <Typography variant="h6">
-                  <a href={senpai.website}>{senpai.website}</a>
-                </Typography>
-              </div>
-            ) : null}
+              {senpai.location ? (
+                <div>
+                  <Typography variant="h5">Location:</Typography>
+                  <Typography variant="h6">{senpai.location}</Typography>
+                </div>
+              ) : null}
+              {senpai.website ? (
+                <div>
+                  <Typography variant="h5">Personal Website:</Typography>
+                  <Typography variant="h6">
+                    <a href={senpai.website}>{senpai.website}</a>
+                  </Typography>
+                </div>
+              ) : null}
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid
-          container
-          xs={8}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-          }}
-        >
-          <Container
-            fixed
-            className={classes.container}
+          <Grid
+            container
+            xs={8}
             style={{
-              padding: "1rem",
-              height: "65vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
             }}
           >
-            <iframe
-              width="854"
-              height="480"
-              src="https://www.youtube.com/embed/dHRO8M6elcQ"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </Container>
-          <Container
-            fixed
-            className={classes.container}
-            style={{ height: "30vh", padding: "2rem" }}
-          >
-            {senpai.bio ? (
-              <Typography variant="h5" component="p">
+            <Container
+              fixed
+              className={classes.container}
+              style={{
+                padding: "1rem",
+                height: "65vh",
+              }}
+            >
+              <iframe
+                width="854"
+                height="480"
+                src="https://www.youtube.com/embed/dHRO8M6elcQ"
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </Container>
+            <Container
+              fixed
+              className={classes.container}
+              style={{ height: "30vh", padding: "2rem" }}
+            >
+              {senpai.bio ? (
+                <Typography variant="h5" component="p">
                 Bio
-              </Typography>
-            ) : null}
-            {senpai.bio ? (
-              <Typography variant="h6" component="p">
-                {senpai.bio}
-              </Typography>
-            ) : null}
-          </Container>
-        </Grid>
-      </Grid>
+                </Typography>
+              ) : null}
+              {senpai.bio ? (
+                <Typography variant="h6" component="p">
+                  {senpai.bio}
+                </Typography>
+              ) : null}
+            </Container>
+          </Grid>
+        </Grid> 
+        : 
+        <CircularProgress />
+      }
+      
     </div>
   );
 }
