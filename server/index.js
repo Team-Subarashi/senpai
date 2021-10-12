9afa8b9a0a0a0db9c0c2f57efc8a1ea55953e6a8
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-10-12 15:47:54 +0900

    socket

--- server/index.js
+++ server/index.js
@@ -34,22 +34,27 @@ app.get('/', (req, res) => {
 
 const io = new Server(server, {
   cors: {
-    origin: "*"
-  }
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
-    socket.join(data)
-    console.log(`User with ID: ${socket.id} joined room: ${data}`)
-  })
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
   socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id)
-  })
-})
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 
 
