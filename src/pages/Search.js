e5ac3c7dc133436d9cfa6b4a4be0f5df6e5063ae
GiveKumaAGun <reijitsusaka@gmail.com>
2021-10-13 22:42:15 +0900

    adjusted senpai-list to scroll since the vanta canvas doesn't stretch

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,37 +2,38 @@ import React from "react";
 import Filter from "../components/Filter";
 import SenpaiList from "../components/SenpaiList";
 import Grid from "@material-ui/core/Grid";
+import Vanta from "../components/Vanta";
 
 export default function Search() {
   return (
-    <Grid container style={{}}>
-      <Grid
-        item
-        xs={12}
-        style={{
-          height: "5vh",
+    <Vanta>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          style={{
+            height: "5vh",
           //   backgroundColor: "red"
-        }}
-      >
-        <Filter />
+          }}
+        >
+          <Filter />
+        </Grid>
+        {/* <Grid item xs={2}></Grid> */}
+        <Grid
+          id="senpai-list"
+          item
+          xs={12}
+          style={{
+            height: "95vh",
+            width: "max-content",
+            padding: "2rem",
+            overflow: "scroll",
+          }}
+        >
+          <SenpaiList />
+        </Grid>
+        {/* <Grid item xs={2}></Grid> */}
       </Grid>
-      {/* <Grid item xs={2}></Grid> */}
-
-      <Grid
-        id="senpai-list"
-        item
-        xs={10}
-        style={{
-          marginLeft: "8.5vw",
-          // height: "80vh",
-          backgroundColor: "#616162",
-          //border: "1px solid white"
-          // overflow: "scroll",
-        }}
-      >
-        <SenpaiList />
-      </Grid>
-      {/* <Grid item xs={2}></Grid> */}
-    </Grid>
+    </Vanta>
   );
 }
