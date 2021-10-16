5e2fe4b25eea7dab94ee06765782d22356f49758
GiveKumaAGun <reijitsusaka@gmail.com>
2021-10-16 16:16:39 +0900

    Feat: updating a lesson now sends emails to senpai and kouhai

--- server/controllers/LessonController.js
+++ server/controllers/LessonController.js
@@ -1,5 +1,16 @@
 const Lesson = require("../models/LessonModel");
+const User = require("../models/userModel");
 require("dotenv").config();
+
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'subarashisenpaiapp@gmail.com',
+    pass: 'subarashi-team'
+  }
+});
 
 let OpenTok = require('opentok');
 let opentok = new OpenTok(process.env.API_KEY, process.env.SECRET);
@@ -43,17 +54,16 @@ exports.getLessonsByKouhaiId = (req, res) => {
 exports.createNewLesson = async (req, res) => {
   await opentok.createSession(function(err, session) {
     if (err) return console.log(err);
-    req.body.vonageSessionId = session.sessionId
+    req.body.vonageSessionId = session.sessionId;
     let newLesson = new Lesson(req.body);
     newLesson.save((err, lesson) => {
       if (err) {
-        console.log
         res.status(500).send(err);
       }
       res.status(201).json(lesson);
     });
   });
-  
+
 };
 // updateTodo function — To update todo status by id
 exports.updateLesson = (req, res) => {
@@ -61,11 +71,49 @@ exports.updateLesson = (req, res) => {
     { _id: req.params.id },
     req.body,
     { new: true },
-    (err, todo) => {
+    async (err, lesson) => {
       if (err) {
         res.status(500).send(err);
       }
-      res.status(200).json(todo);
+
+      const senpai = await User.findById(lesson.senpaiId);
+      const kouhai = await User.findById(req.body.kouhaiId);
+
+      let toSenpai = {
+        from: 'subarashisenpaiapp@gmail.com',
+        to: senpai.email,
+        subject: `New Lesson with ${kouhai.name}`,
+        text: `Time: ${new Date(lesson.startDate)}`
+      };
+
+      transporter.sendMail(toSenpai, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          console.log(toSenpai.to);
+
+        }
+      });
+
+      let toKouhai = {
+        from: 'subarashisenpaiapp@gmail.com',
+        to: kouhai.email,
+        subject: `Lesson booked with ${senpai.name}`,
+        text: `Time: ${new Date(lesson.startDate)}`
+      };
+
+      transporter.sendMail(toKouhai, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+          console.log(toKouhai.to);
+        }
+      });
+
+
+      res.status(200).json(lesson);
     }
   );
 };
